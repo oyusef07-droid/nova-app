de233b4e3bfdc6a3cbe1ad7a02b6c1069f828e9c
oyusef07 <oyusef07@gmail.com>
2026-06-05 21:38:10 +0300

    Fix Vercel deploy: Force execution of build script using @vercel/static-build and deploy Hono server using @vercel/node

--- api/server.js
+++ api/server.js
@@ -1,12 +1,7 @@
-process.env.PORT = '0'; // Listen on a random ephemeral port to avoid EADDRINUSE
+import server from '../build/server/index.js';
 
-export default async function handler(req, res) {
-  try {
-    const serverModule = await import('../build/server/index.js');
-    const server = serverModule.default;
-    server.emit('request', req, res);
-  } catch (err) {
-    console.error("Error loading server:", err);
-    res.status(500).send("Internal Server Error loading Hono");
-  }
+process.env.PORT = '0';
+
+export default function handler(req, res) {
+  server.emit('request', req, res);
 }
